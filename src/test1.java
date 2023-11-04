279f60e9d350897e89c15430a348d60c734086dc
kayahan <kskrev@gmail.com>
2023-11-04 12:05:34 +0200

    branch1-com

--- src/test1.java
+++ src/test1.java
@@ -2,7 +2,7 @@ public class test1 {
 
     public void  test(){
         System.out.println("test branch 1");
-        System.out.println("Hello Faho");
+        System.out.println("Hello Faho, yo yo yo");
         System.out.println();
 
     }
